7c41bda8a786194139cfcce66fcfe1d5056e1339
adex720 <25402611+adex720@users.noreply.github.com>
2024-07-11 13:23:22 +0300

    Cleanup code

--- src/main/java/com/adex/fluxbot/discord/command/game/CommandCreate.java
+++ src/main/java/com/adex/fluxbot/discord/command/game/CommandCreate.java
@@ -16,14 +16,14 @@ public class CommandCreate extends Command {
     public void execute(EventContext context) {
         SlashCommandInteractionEvent event = context.getSlashCommandEvent();
         if (context.inGame()) {
-            event.replyEmbeds(MessageCreator.createDefault("New game", "Cannot create game"
-                    , "You are already in a game")).setEphemeral(true).queue();
+            event.replyEmbeds(MessageCreator.createDefault("New game", "Cannot create game",
+                    "You are already in a game")).setEphemeral(true).queue();
             return;
         }
 
         long userId = context.getUserId();
         FluxGame game = context.getBot().getGameManager().createGame(context.getBot(), userId, context.getUsername(), context.getChannel());
-        event.replyEmbeds(MessageCreator.createDefault("New game", context.getUsername() + " created a new game"
-                , "Invite users with /invite\nJoin with /join " + context.getUserAsMention())).queue();
+        event.replyEmbeds(MessageCreator.createDefault("New game", context.getUsername() + " created a new game",
+                "Invite users with /invite\nJoin with /join " + context.getUserAsMention())).queue();
     }
 }
